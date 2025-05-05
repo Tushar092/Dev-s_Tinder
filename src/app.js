e2ce92b894a2c06f354f4085ae7ac01d1ad2bcdd
Tushar <karansinghrajora1996@gmail.com>
2025-05-06 00:54:43 +0530

    Added different HTTP methods to the respective routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
+app.get("/hello", (req, res) => {
     res.send("Hello from the server");
 });
 
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
     res.send("Hi");
 });
 
